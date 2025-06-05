470ab62a7d06025934f532b3acce38d26c7bbdf3
IsNina1 <kaidan0902@gmail.com>
2025-06-05 11:30:56 +0900

    Add files via upload

---
+++ Timetable website 6.5/db.js
@@ -0,0 +1,11 @@
+// db.js
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'user_db',
+  waitForConnections: true,
+  connectionLimit: 10
+});
+module.exports = pool;
